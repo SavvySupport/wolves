2ff9b6831dbc3d9c5123e7f7c9d2a5227021e950
thang.arsenal <thang.arsenal@gmail.com>
2016-01-27 00:47:50 +1100

    Added test code for handling upload image files with ajax. tested and worked

--- app/views.py
+++ app/views.py
@@ -8,9 +8,12 @@ from app.Forms.recover import recoverForm
 from app.Forms.account import employerForm, candidateForm
 from app.Forms.viewProfile import viewProfile
 from app.Models.User import User
+from app.Helpers.FileHelper import FileHelper
 from jinja2 import Environment, FileSystemLoader
+from werkzeug import secure_filename
 from flask import Flask, request, session, g, redirect, url_for, \
-                    abort, render_template, flash
+                    abort, render_template, flash, jsonify, \
+                    send_from_directory
 from flask.ext.login import login_user, logout_user, login_required, current_user
 
 # Define the template directory
@@ -18,7 +21,6 @@ tpldir = os.path.dirname(os.path.abspath(__file__))+'/templates/'
 
 # Setup the template enviroment
 env = Environment(loader=FileSystemLoader(tpldir), trim_blocks=True)
-
 ###############################################################################
 # Login Manager
 ###############################################################################
@@ -37,7 +39,7 @@ def load_user(username):
 ###############################################################################
 @app.errorhandler(404)
 def not_found(error):
-    return render('404.html', 404)
+    return render('404.html', error=404)
 
 def render(page, form=None, error=None):
     if error:
@@ -57,10 +59,45 @@ def render(page, form=None, error=None):
 def home():
     return render('home.html')
 
-@app.route('/test/<x>')
+@app.route('/testajax')
+def testajax():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a+b)
+
+@app.route('/uploadajax/<filename>')
+def uploaded_file(filename):
+    upload_folder = url_for('static', filename='/user/')
+    return send_from_directory(upload_folder, filename)
+
+@app.route('/uploadajax', methods=['POST'])
+def uploadajax():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            # Get filename
+            filename = secure_filename(file.filename)
+
+            # Set up upload folder
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            updir = os.path.join(basedir, 'static/images/user/')
+
+            # upload file to server
+            path = os.path.join(updir, filename)
+            file.save(path)
+
+            # return information to frontend
+            file_size = os.path.getsize(os.path.join(updir, filename))
+            return jsonify(name=filename, size=file_size, path=os.path.join(url_for('static', filename='images/user/'), filename))
+
+def allowed_file(filename):
+    app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/test')
 @login_required
-def test(x):
-    return render('home.html')
+def test():
+    return render('test.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -89,7 +126,7 @@ def logout():
 def activate(username, token):
     if User.validate_rego_token(username, token):
         login_user(User(username))
-        return redirect(url_for('account', account = username))
+        return redirect(url_for('account'))
     return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -101,7 +138,7 @@ def register():
     if request.method == 'POST' and form.validate():
         return redirect(url_for('account'))
 
-    return render('register.html', form)
+    return render('register.html', form=form)
 
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
@@ -125,7 +162,7 @@ def account():
             else:
                 flash('Failed to update your profile', 'error')
 
-        return render('account.html', form)
+        return render('account.html', form=form)
     else:
         flash('Invalid access to account', 'error')
         return redirect(url_for('home'))
