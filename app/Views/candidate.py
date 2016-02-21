235b72f02aeea772c115cbd29c60241725b865ad
thang.arsenal <thang.arsenal@gmail.com>
2016-02-22 00:23:31 +1100

    added link to view candidate profile from search

--- app/Views/candidate.py
+++ app/Views/candidate.py
@@ -1,20 +1,14 @@
 from app import app, savvy_collection, jobs_collection
 from app.Views.views import render
-from app.Forms.account import candidateForm
-from flask.ext.login import login_user, login_required
 from flask import request, redirect, url_for, flash
-from flask.ext.login import login_user, current_user
 from app.Helpers.Constant import *
+from bson.objectid import ObjectId
 
-@app.route('/candidate/<email>', methods=['GET', 'POST'])
-def candidate(email):
-    user = savvy_collection.find_one({ EMAIL: email })
+@app.route('/candidate/<uid>', methods=['GET'])
+def candidate(uid):
+    user = savvy_collection.find_one({ '_id': ObjectId(uid) })
     if user and user.get(CATEGORY) == CAND:
-        form = candidateForm(user, request.form)
-        if request.method == 'GET':
-            form.prepopulate(user)
-
-        return render('candidate.html', form=form, extra=user)
+        return render('candidate.html', extra=user)
     else:
         flash('Invalid access to account', 'error')
         return redirect(url_for('home'))
